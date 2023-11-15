f787f8b7e7f2500ce2ebdce6b6dedcfc3edeb9f2
Ali-ammar-1997 <67014427+Ali-ammar-1997@users.noreply.github.com>
2023-11-15 19:09:12 +0300

    Price list

---
+++ price_list_qutation/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2018 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Price list extension",
+    "summary": """ """,
+    "version": "16.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "",
+    "depends": ["sale"],
+    "data": [
+             "views/price_list.xml"],
+}
